70d46a21616fe9edc03ded6590c6d073f3193dc5
Reed, Brian <brian.reed@redprairie.com>
2014-04-29 14:53:07 -0500

    Increasing java heap size.

--- slave/swarm.go
+++ slave/swarm.go
@@ -1,62 +1,62 @@
 package slave
 
 import (
-        "fmt"
-        "os/exec"
-        "strconv"
+	"fmt"
+	"os/exec"
+	"strconv"
 )
 
 // Swarm is an object that represents a Jenkins swarm client connector for connecting to a Jenkins master.
 type Swarm struct {
-        // Ch is a channel to send a value on to force a restart.
-        Ch chan bool
+	// Ch is a channel to send a value on to force a restart.
+	Ch chan bool
 }
 
 func (s *Swarm) Url() string {
-        return fmt.Sprintf("http://maven.jenkins-ci.org/content/repositories/releases/org/jenkins-ci/plugins/swarm-client/%s/%s", spec.Swarmversion, s.File())
+	return fmt.Sprintf("http://maven.jenkins-ci.org/content/repositories/releases/org/jenkins-ci/plugins/swarm-client/%s/%s", spec.Swarmversion, s.File())
 }
 
 func (s *Swarm) File() string {
-        return fmt.Sprintf("swarm-client-%s-jar-with-dependencies.jar", spec.Swarmversion)
+	return fmt.Sprintf("swarm-client-%s-jar-with-dependencies.jar", spec.Swarmversion)
 }
 
 func (s *Swarm) Overwrite() bool {
-        return false
+	return false
 }
 
 func (s *Swarm) Command() *exec.Cmd {
-        env, _ := NewSpec().Environment()
+	env, _ := NewSpec().Environment()
 
-        args := []string{"-XX:MaxPermSize=128m", "-jar", s.File()}
+	args := []string{"-XX:MaxPermSize=256m", "-jar", s.File()}
 
-        if len(env.Username) > 0 {
-                args = append(args, "-username", env.Username)
-        }
+	if len(env.Username) > 0 {
+		args = append(args, "-username", env.Username)
+	}
 
-        if len(env.Password) > 0 {
-                args = append(args, "-password", env.Password)
-        }
+	if len(env.Password) > 0 {
+		args = append(args, "-password", env.Password)
+	}
 
-        if len(env.Server) > 0 {
-                args = append(args, "-master", env.Server)
-        }
+	if len(env.Server) > 0 {
+		args = append(args, "-master", env.Server)
+	}
 
-        if len(env.Labels) > 0 {
-                args = append(args, "-labels", env.Labels)
-        }
+	if len(env.Labels) > 0 {
+		args = append(args, "-labels", env.Labels)
+	}
 
-        args = append(args,
-                "-mode",
-                env.Mode,
-                "-name",
-                env.Name,
-                "-executors",
-                strconv.Itoa(env.Executors),
-        )
+	args = append(args,
+		"-mode",
+		env.Mode,
+		"-name",
+		env.Name,
+		"-executors",
+		strconv.Itoa(env.Executors),
+	)
 
-        return exec.Command("java", args...)
+	return exec.Command("java", args...)
 }
 
 func (s *Swarm) Restart() chan bool {
-        return s.Ch
+	return s.Ch
 }
--- slave/swarm_test.go
+++ slave/swarm_test.go
@@ -1,70 +1,70 @@
 package slave
 
 import (
-        "testing"
+	"testing"
 )
 
 func TestSwarmUrl(t *testing.T) {
-        NewSpec().Environment()
-        s := &Swarm{}
+	NewSpec().Environment()
+	s := &Swarm{}
 
-        exp := "http://maven.jenkins-ci.org/content/repositories/releases/org/jenkins-ci/plugins/swarm-client/1.15/swarm-client-1.15-jar-with-dependencies.jar"
-        if url := s.Url(); url != exp {
-                t.Log("Expected: %s, Got: %s", exp, url)
-        }
+	exp := "http://maven.jenkins-ci.org/content/repositories/releases/org/jenkins-ci/plugins/swarm-client/1.15/swarm-client-1.15-jar-with-dependencies.jar"
+	if url := s.Url(); url != exp {
+		t.Log("Expected: %s, Got: %s", exp, url)
+	}
 
-        spec = nil
+	spec = nil
 }
 
 func TestSwarmOverwrite(t *testing.T) {
-        s := &Swarm{}
+	s := &Swarm{}
 
-        if s.Overwrite() {
-                t.Log("Expected false, Got true.")
-                t.Fail()
-        }
+	if s.Overwrite() {
+		t.Log("Expected false, Got true.")
+		t.Fail()
+	}
 }
 
 func TestSwarmCommand(t *testing.T) {
-        s := &Swarm{}
+	s := &Swarm{}
 
-        exp := []string{
-                "java",
-                "-XX:MaxPermSize=128m",
-                "-jar",
-                "swarm-client-1.15-jar-with-dependencies.jar",
-                "-username",
-                "testuser",
-                "-password",
-                "testpass",
-                "-master",
-                "http://test",
-                "-labels",
-                "label1 label2",
-                "-mode",
-                "normal",
-                "-name",
-                "testname",
-                "-executors",
-                "2",
-        }
-        env, _ := NewSpec().Environment()
-        env.Username = "testuser"
-        env.Password = "testpass"
-        env.Server = "http://test"
-        env.Labels = "label1 label2"
-        env.Name = "testname"
+	exp := []string{
+		"java",
+		"-XX:MaxPermSize=256m",
+		"-jar",
+		"swarm-client-1.15-jar-with-dependencies.jar",
+		"-username",
+		"testuser",
+		"-password",
+		"testpass",
+		"-master",
+		"http://test",
+		"-labels",
+		"label1 label2",
+		"-mode",
+		"normal",
+		"-name",
+		"testname",
+		"-executors",
+		"2",
+	}
+	env, _ := NewSpec().Environment()
+	env.Username = "testuser"
+	env.Password = "testpass"
+	env.Server = "http://test"
+	env.Labels = "label1 label2"
+	env.Name = "testname"
 
-        cmd := s.Command()
+	cmd := s.Command()
 
-        if len(cmd.Args) != len(exp) {
-                t.Errorf("Wrong number of args: %d, Exp: %d", len(cmd.Args), len(exp))
-                return
-        }
+	if len(cmd.Args) != len(exp) {
+		t.Errorf("Wrong number of args: %d, Exp: %d", len(cmd.Args), len(exp))
+		return
+	}
 
-        for i, v := range cmd.Args {
-                if v != exp[i] {
-                        t.Errorf("Expected: %s, Got: %s", exp[i], v)
-                }
-        }
+	for i, v := range cmd.Args {
+		if v != exp[i] {
+			t.Errorf("Expected: %s, Got: %s", exp[i], v)
+		}
+	}
 }
